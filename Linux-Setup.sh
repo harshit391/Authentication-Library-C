826731d04ad8a319c002d202c40411a898715340
Harshit Singla <harshitsingla1761@gmail.com>
2024-09-05 12:52:51 +0530

    Updated Linux-Setup File and Create Auth Setup Empty File
    Forgot to move the text files into database folder after creation

--- Linux-Setup.sh
+++ Linux-Setup.sh
@@ -18,19 +18,25 @@ sudo apt-get install pkg-config
 echo "Enter your email address: "
 read email
 echo $email > email.txt
+mv email.txt database/files
 
 # Take input for App Appword for Gmail and saving it as text file in database folder
 echo "Enter your App Password: "
 read appword
 echo $appword > appword.txt
+mv appword.txt database/files
 
 # Take input for MongoDb URL and saving it as text file in database folder
 echo "Enter your MongoDb URL: "
 read url
 echo $url > mongouri.txt
+mv mongouri.txt database/files
 
 # Ending by printing the message
 echo "Setup Completed Successfully"
 
 # End of the Script
 echo "Please Run Command: ./app.sh to run the application" 
+echo "To Build the Static Library for whole Authentication System Run Command: ./Auth-Setup.sh"
+
+echo "Thank You"
